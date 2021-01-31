afdd057873e05b1fcf535a0eeb10dac590589e7a
dawei <dawei.chen@hotmail.co.uk>
2021-01-31 13:16:28 +0000

    a

--- extensa/misc/devdeps.sh
+++ extensa/misc/devdeps.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
 
+_golang(){
+    local d="$GOPATH"
+    local prefix='d:\godev'
+    [ "$d" = 'd:\godev' ]||exit 1
+    [ -d "$d" ]||mkdir "$d"
 
-go get 'golang.org/x/tools/gopls'
-go get 'github.com/uudashr/gopkgs/v2/cmd/gopkgs'
-go get 'github.com/ramya-rao-a/go-outline'
-go get 'github.com/go-delve/delve/cmd/dlv'
-go get 'golang.org/x/lint/golint'
+    go get 'golang.org/x/tools/gopls'
+    go get 'github.com/uudashr/gopkgs/v2/cmd/gopkgs'
+    go get 'github.com/ramya-rao-a/go-outline'
+    go get 'github.com/go-delve/delve/cmd/dlv'
+    go get 'golang.org/x/lint/golint'
+}
 
+_golangclear()(
+    [ ! -d 'd:\godev' ]||set -x;rm -rf 'd:\godev'
+)
 
+_usage(){
+    cat<<-EOF
+	SYNOPSIS:
+	    $0 --golang
+	    $0 --golangclear
+	EOF
+}
+
+case "${1}" in
+    --golang)_golang;;
+    --golangclear)_golangclear;;
+    *)_usage;;
+esac
 
 
 
