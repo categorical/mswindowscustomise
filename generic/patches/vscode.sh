b9e134095641cd1231954f876df84ccfacd3ca8b
dawei <dawei.chen@hotmail.co.uk>
2021-10-18 03:15:46 +0100

    a

--- generic/patches/vscode.sh
+++ generic/patches/vscode.sh
@@ -6,12 +6,25 @@ f="$progdir/vscode/code.exe"
 
 proxy=${http_proxy}
 
+_start(){
 if [ -z "$proxy" ];then
-    "$f" "$@" 2>&1 >/dev/null &
+    #"$f" "$@" 2>&1 >/dev/null &
+    cygstart "$f" "$@"
 else
     "$f" --proxy-server="${proxy/:\/\//=}" "$@" 2>&1 >/dev/null &
+    
 fi
+}
 
+_stop(){
+    taskkill /im code.exe /f
+    :
+}
+
+case $1 in
+    --stop)_stop;;
+    *)_start "$@";;
+esac
 
 
 
