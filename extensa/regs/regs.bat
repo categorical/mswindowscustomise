ee447d609e18356b9bceef17f8e06a61037d8203
dawei <dawei.chen@hotmail.co.uk>
2021-10-23 12:46:05 +0100

    a

--- extensa/regs/regs.bat
+++ extensa/regs/regs.bat
@@ -19,6 +19,7 @@ set "mspath=%mspath%%%USERPROFILE%%\AppData\Local\Microsoft\WindowsApps;"
 set "mspath=%mspath%d:\bin;"
 set "mspath=%mspath%d:\sbin;"
 set "mspath=%mspath%d:\programs\portablegit\bin;"
+set "mspath=%mspath%d:\programs\jdk\bin;"
 ::reg add "HKEY_CURRENT_USER\Environment" /v Path /d "%mspath%" /f
 setx "Path" "%mspath%"
 reg query "HKEY_CURRENT_USER\Environment" /v Path
