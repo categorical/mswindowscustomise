ec347a085852d2ad667b5ba146f7df2488ac2e42
dawei <dawei.chen@hotmail.co.uk>
2022-03-20 19:34:49 +0100

    update

--- backlog/msnonsense.sh
+++ backlog/msnonsense.sh
@@ -17,11 +17,22 @@ _kill(){
     done< <(find "$(cygpath -D)" "$(cygpath -DA)" -mindepth 1 -maxdepth 1)
 }
 
+_clean(){
+    declare -a vs=(
+        'd:\msdownld.tmp'
+    )
+    for v in "${vs[@]}";do v="$(cygpath -u "$v")"
+        test -f "$v"&&(set -x;rm "$v")
+        test -d "$v"&&(set -x;rm -r "$v")
+    done
+}
+
 _usage(){
     cat<<-EOF
 	SYNOPSIS
 	    $0 -l|--list
 	    $0 -k|--kill
+	    $0 -c|--clean
 	EXAMPLES
 	    $0 -l -k -l
 	EOF
@@ -31,6 +42,7 @@ _usage(){
 while [ $# -gt 0 ];do case $1 in
     -l|--list)_list;;
     -k|--kill)_kill;;
+    -c|--clean)_clean;;
     *)_usage 0;;
 esac;shift;done
 
