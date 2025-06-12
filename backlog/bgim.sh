c47c75765e824369e23df936c5bac0fd60dc3034
dawei <dawei.chen@hotmail.co.uk>
2025-06-12 04:31:05 +0100

    wip

--- backlog/bgim.sh
+++ backlog/bgim.sh
@@ -7,6 +7,7 @@ thisdir=$(cd "$(dirname "$0")" && pwd)
 
 c1='hklm\software\policies\microsoft\windows\personalization'
 c2='hkcu\control panel\desktop'
+c3='hklm\software\policies\microsoft\windows\system'
 rst='c:\windows\web\wallpaper\windows\img0.jpg'
 setv(){
     in="${in-$rst}"
@@ -16,6 +17,34 @@ setv(){
 setlck(){
     sudo reg add "$c1" /f /d "$in" /t reg_sz /v lockscreenimage
 }
+setlck2(){
+# lock bg
+# -------
+# 1 depending on state of microsoft, `lockscreenimage` MIGHT work
+# 1 if no work, lock bg is per user at
+#   `c:\programdata\microsoft\windows\systemdata\UID\readonly`
+#   unless `nochanginglockscreen`
+#
+# login bg
+# --------
+# 1 login uses lock bg unless `disablelogonbackgroundimage`
+case ${undo-} in t)
+reg delete "$c1" /f /v lockscreenimage
+reg delete "$c1" /f /v nochanginglockscreen
+reg delete "$c1" /f /v nolockscreen
+reg delete "$c3" /f /v disablelogonbackgroundimage
+return;esac
+reg add "$c1" /f /d "$in" /t reg_sz /v lockscreenimage
+reg add "$c1" /f /d 1 /t reg_dword  /v nochanginglockscreen
+reg add "$c1" /f /d 1 /t reg_dword  /v nolockscreen
+reg add "$c3" /f /d 0 /t reg_dword  /v disablelogonbackgroundimage
+}
+get2(){
+reg query "$c1" /v nochanginglockscreen
+reg query "$c1" /v nolockscreen
+reg query "$c3" /v disablelogonbackgroundimage
+}
+
 setbg(){
 # microsoft relies on this newline
 powershell -c -< <(cat<<EOF|sed '$s/$/\x0a/'
@@ -44,20 +73,20 @@ get(){
 }
 _main(){ _usage(){ cat<<-EOF
 	SYNOPSIS:
-	    $0 -setbg|-setlck [-in IM]
-	    $0 -get
+	    $0 -setbg|-setlck|-setlck2 [-in IM]
+	    $0 -get|-get2
 	EXAMPLE
 	    $0 -setbg -in 'c:\windows\web\screen\img103.png'
 	EOF
     exit $1;}
     declare -a a;while [ $# -gt 0 ];do case $1 in
         -in)shift;in="$1";;
+        -undo)undo=t;;
         *)a+=("$1")
     esac;shift;done;setv;set -- "${a[@]}"
     [ $# -gt 0 ]||_usage 1;while [ $# -gt 0 ];do case $1 in
-        -setbg)setbg;;
-        -setlck)setlck;;
-        -get)get;;
+        -setbg)setbg;;-setlck)setlck;;-get)get;;
+        -setlck2)setlck2;;-get2)get2;;
         -h)_usage 0;;*)_usage 1
     esac;shift;done
 };_main "$@"
